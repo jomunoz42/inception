845efac3f1e39734388851c371877ba6f51f57aa
jomunoz42 <jpmunoz@sapo.pt>
2026-06-01 22:31:59 +0100

    Created secrets for the passes, added the correct mental model png, studied more

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
+
 mkdir -p /var/www/html
 
 cd /var/www/html
